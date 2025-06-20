65386dddaad38306c43cf58b16a20ed7af63baab
Ting-Hsu Chang <wanini.klp.mashimaro@gmail.com>
2025-06-20 12:34:57 +0800

    fix: improve illegal response handling for better UX

--- samples/__main__.py
+++ samples/__main__.py
@@ -12,18 +12,24 @@ import re
 from _libs import CondaInfo, ProjectInfo, gen_project  # pylint: disable=import-error
 
 # receive the path of this project.
-path = input("Please input the path of this project: ")
-if not os.path.exists(path):
-    raise FileNotFoundError(f"No such file or directory: '{path}'")
+while True:
+    path = input("Please input the path of this project: ")
+    if not os.path.exists(path):
+        print(f"No such file or directory: '{path}'")
+        continue
+    break
 
 # receive the Github url of this project
 
 GITHUB_URL_PAT = re.compile(r'https://github.com/'
                             r'[a-zA-Z_]+[a-zA-Z0-9\-_]*/'
                             r'[a-zA-Z_]+[a-zA-Z0-9\-_]*')
-url = input("Please input the Github url of this project: ")
-if not GITHUB_URL_PAT.match(url):
-    raise ValueError(f"illegal Github url: '{url}'")
+while True:
+    url = input("Please input the Github url of this project: ")
+    if not GITHUB_URL_PAT.match(url):
+        print(f"illegal Github url: '{url}'")
+        continue
+    break
 
 # receive the name of this project
 PROJECT_NAME_PAT = re.compile(r'[a-zA-Z]+[a-zA-Z0-9\-_]*')
@@ -32,46 +38,53 @@ if not PROJECT_NAME_PAT.match(name):
     raise ValueError(f"illegal project name: '{name}'")
 
 # receive the One-line description of this project
-description = input("Please input the One-line description of this project: ")
-if '\n' in description:
-    raise ValueError(f"illegal One-line description: '{description}'")
+while True:
+    description = input("Please input the One-line description of this project: ")
+    if '\n' in description:
+        print(f"illegal One-line description: '{description}'")
+        continue
+    break
 
 # receive the package name of this project
 LIB_NAME_PAT = re.compile('[a-z]+[a-z0-9_]*')
-package = input("Please input the package name of this project: ")
-if not LIB_NAME_PAT.match(package):
-    raise ValueError(f"illegal package name: '{package}'")
+while True:
+    package = input("Please input the package name of this project: ")
+    if not LIB_NAME_PAT.match(package):
+        print(f"illegal package name: '{package}'")
+        continue
+    break
 
 # receive the author of this project
 author = input("Please input the name of author of this project: ")
 
 # receive the author email of this project
 EMAIL_PAT = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
-email = input("Please input the email of author of this project: ")
-if not EMAIL_PAT.match(email):
-    raise ValueError(f"illegal email: '{email}'")
+while True:
+    email = input("Please input the email of author of this project: ")
+    if not EMAIL_PAT.match(email):
+        print(f"illegal email: '{email}'")
+        continue
+    break
 
 # receive the python version of this project
 PYTHON_VERSION_PAT = re.compile(r'3(\.[1-9]+0?){1,2}')
-version = input("Please input the version of Python of this project: ")
-if not PYTHON_VERSION_PAT.match(version):
-    raise ValueError(f"illegal Python version: '{version}'")
+while True:
+    version = input("Please input the version of Python of this project: ")
+    if not PYTHON_VERSION_PAT.match(version):
+        print(f"illegal Python version: '{version}'")
+        continue
+    break
 
 # with conda environment
-with_conda_env = None
-while with_conda_env is None:
+while True:
     recv = input("Do you want to develop with conda environment(Y/N): ")
     if recv == 'Y':
-        with_conda_env = True
+        conda_info = CondaInfo.make(name, version)
         break
     if recv == 'N':
-        with_conda_env = False
+        conda_info = None
         break
     print("invalid input!!")
-
-conda_info = None
-if with_conda_env:
-    conda_info = CondaInfo.make(name, version)
 
 info = ProjectInfo(path=path, url=url, name=name, description=description,
                    package=package, author=author, email=email, version=version,
