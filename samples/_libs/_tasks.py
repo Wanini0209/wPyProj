6c921295d526084a683de317b84632f5ba65546d
Ting-Hsu Chang <Ting.Hsu.Chang@Gmail.com>
2025-12-05 14:13:32 +0800

    fix(gen): sync _common.py generator with coding style standards
    Update the `_common` generator in `samples/_libs/_tasks.py` to match the
    strict coding style enforcement in `tasks/_common.py`.
    The generated file now includes:
    - NumPy-style docstrings.
    - Proper type hints.
    - Correct variable naming conventions.

--- samples/_libs/_tasks.py
+++ samples/_libs/_tasks.py
@@ -22,14 +22,29 @@ _COPY_FILES = [
 def _common(info: ProjectInfo) -> str:
     """Content-Generator for `_common.py` module."""
     ret = (
-        '"""Common definitions"""\n\n'
-        "import sys\n\n"
-        "VENV_PREFIX = 'pipenv run'\n"
-        f"_COMMON_TARGETS = ['{info.package}', 'tests', 'setup.py', 'tasks']\n"
-        "COMMON_TARGETS_AS_STR = ' '.join(_COMMON_TARGETS)\n"
-        f'TEST_TARGET = "{info.package}"\n\n'
-        "# The differences between Linux and Windows\n"
-        "USE_PTY = sys.platform != 'win32'\n"
+        '"""Common definitions for invoke tasks.\\n\\n'
+        "This module contains common constants and configurations used across\\n"
+        "all task modules.\\n\\n"
+        "Attributes\\n"
+        "----------\\n"
+        "VENV_PREFIX : str\\n"
+        "    Prefix command for running commands in virtual environment.\\n"
+        "COMMON_TARGETS : list of str\\n"
+        "    List of common target directories for various tasks.\\n"
+        "COMMON_TARGETS_AS_STR : str\\n"
+        "    Space-separated string of common targets.\\n"
+        "TEST_TARGET : str\\n"
+        "    Default target directory for testing.\\n"
+        "USE_PTY : bool\\n"
+        "    Whether to use pseudo-terminal based on platform.\\n\\n"
+        '"""\\n\\n'
+        "import sys\\n\\n"
+        'VENV_PREFIX: str = "pipenv run"\\n'
+        f'COMMON_TARGETS: list[str] = ["{info.package}", "tests", "setup.py", "tasks"]\\n'  # noqa: E501
+        'COMMON_TARGETS_AS_STR: str = " ".join(COMMON_TARGETS)\\n'
+        f'TEST_TARGET: str = "{info.package}"\\n\\n'
+        "# The differences between Linux and Windows\\n"
+        'USE_PTY: bool = sys.platform != "win32"\\n'
     )
     return ret
 
